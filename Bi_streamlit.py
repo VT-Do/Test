853c769cfa2e4d7bf74a93941c4b4a34830a6f9c
VT-Do <115161122+VT-Do@users.noreply.github.com>
2022-10-06 22:14:02 +0200

    Update Bi_streamlit.py

--- Bi_streamlit.py
+++ Bi_streamlit.py
@@ -66,7 +66,7 @@ if choice=="WEB":
 	menu_SellerDomain=['All']+df['SellerDomain'].unique().tolist()
 	choice1=st.sidebar.selectbox("Seller Domain", menu_SellerDomain)
 
-	df= df[((df['AdvertisingSystem'] == choice_AdvertisingSytem)|(choice_AdvertisingSytem=='All')) & ((df['PubAccId'] == choice_PubAccId )|(choice_PubAccId=='All') ) ]
+	df= df[((df['AdvertisingSystem'] ==choice_AdvertisingSystem ) | (choice_AdvertisingSystem=='All'))  ]
 	
 	
 	st.dataframe(df, width=None, height=1000)
