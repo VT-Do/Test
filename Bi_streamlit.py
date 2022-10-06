1c0bc069c4cfb9dbd121cd55820e0b1211d370de
VT-Do <115161122+VT-Do@users.noreply.github.com>
2022-10-06 15:44:57 +0200

    Add files via upload

--- Bi_streamlit.py
+++ Bi_streamlit.py
@@ -1,17 +1,44 @@
-import streamlit as st
+#!/usr/bin/env python
+# coding: utf-8
 
-st.set_page_config(page_title="The Ramsey Highlights", layout="wide")
-st.markdown(
+# In[ ]:
+
+
+import streamlit as st
+import pandas as pd
+# import dask.dataframe as dd
+# from dask import delayed, compute
+import warnings
+import requests
+import gzip
+import shutil
+#from google.cloud import bigquery
+import os
+from google.cloud.bigquery.client import Client
+import smtplib
+#from pandas.core.common import SettingWithCopyWarning
+#warnings.simplefilter(action="ignore", category=SettingWithCopyWarning)
+#import json,urllib.request
+#import concurrent.futures
+import time
+
+
+st.write("""# My first app 
+Hello *world!*""")
+
+# Load table from BQ
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "showheroes-bi-4be202b7b571-cloud.json"
+
+# Construct a BigQuery client object.
+client = bigquery.Client()
+query = """
+        SELECT *
+        FROM `showheroes-bi.bi.bi_list_of_domains`
     """
-    <style>
-    [data-testid="stSidebar"][aria-expanded="true"] > div:first-child{
-        width: 400px;
-    }
-    [data-testid="stSidebar"][aria-expanded="false"] > div:first-child{
-        width: 400px;
-        margin-left: -400px;
-    }
-     
-    """,
-    unsafe_allow_html=True,
-)
+query_job = client.query(query)  # Make an API request.
+    
+# import querry_job as a dataframe
+df=client.query(query).to_dataframe()
+
+st.table(df)
+
