e6ccd4a13bf73585f2a234dc803ca0c517d9d39d
VT-Do <115161122+VT-Do@users.noreply.github.com>
2022-10-06 17:51:02 +0200

    Update Bi_streamlit.py

--- Bi_streamlit.py
+++ Bi_streamlit.py
@@ -68,6 +68,15 @@ elif choice=="APP":
 	query="SELECT * FROM `showheroes-bi.bi.bi_appadstxt_join_sellersjson_with_count_domains` limit 10000"
 	query_job = client.query(query)
 	df=client.query(query).to_dataframe()
+	
+	menu_AdversitingSytem=['All']+df['AdvertisingSystem'].to_list()
+	choice_AdvertisingSystem=st.sidebar.selectbox("Advertising System", menu_AdversitingSytem)
+	
+	menu_PubAccId=['All']+df['PubAccId'].to_list()
+	choice_PubAccId=st.sidebar.selectbox("Publisher Account ID", menu_PubAccId)
+	
+	menu_SellerDomain=['All']+df['SellerDomain'].to_list()
+	choice1=st.sidebar.selectbox("Seller Domain", menu_SellerDomain)
 	st.dataframe(df, width=None, height=1000)
 else:
 	# Store the initial value of widgets in session state
