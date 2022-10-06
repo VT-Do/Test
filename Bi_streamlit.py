cab8f09107de4703337d465966bd5458fbd9264b
VT-Do <115161122+VT-Do@users.noreply.github.com>
2022-10-06 15:55:00 +0200

    Update Bi_streamlit.py

--- Bi_streamlit.py
+++ Bi_streamlit.py
@@ -8,11 +8,11 @@ st.markdown(
     """
     <style>
     [data-testid="stSidebar"][aria-expanded="true"] > div:first-child{
-        width: 400px;
+        width: 350px;
     }
     [data-testid="stSidebar"][aria-expanded="false"] > div:first-child{
-        width: 400px;
-        margin-left: -400px;
+        width: 350px;
+        margin-left: -350px;
     }
      
     """,
