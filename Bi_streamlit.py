821cc72b303f27c98f224244bb1502fd26925611
VT-Do <115161122+VT-Do@users.noreply.github.com>
2022-10-06 18:23:57 +0200

    Update Bi_streamlit.py

--- Bi_streamlit.py
+++ Bi_streamlit.py
@@ -79,8 +79,8 @@ elif choice=="APP":
 	choice1=st.sidebar.selectbox("Seller Domain", menu_SellerDomain)
 	
 	#df= df[(df['AdvertisingSystem'] == menu_AdversitingSytem) & (df['PubAccId'] == menu_PubAccId ) ]
-	st.write(menu_AdversitingSytem)
-	st.write(menu_PubAccId)
+	st.write(st.write('You selected:', menu_AdversitingSytem)
+	
 	
 	st.dataframe(df, width=None, height=1000)
 	st.write(menu_AdversitingSytem)
