0c55b45d6d9eeee2ed2011cf018770e83a3d32fb
VT-Do <115161122+VT-Do@users.noreply.github.com>
2022-10-07 22:24:33 +0200

    Update Bi_streamlit.py

--- Bi_streamlit.py
+++ Bi_streamlit.py
@@ -116,3 +116,4 @@ else:
 
 	with col2:
     		option = st.selectbox("How would you like to be contacted?",("Email", "Home phone", "Mobile phone"),label_visibility=st.session_state.visibility,disabled=st.session_state.disabled,)
+	"""
