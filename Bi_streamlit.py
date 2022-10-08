cf63438e91f181298be086011470f6ebda9e846a
VT-Do <115161122+VT-Do@users.noreply.github.com>
2022-10-08 10:04:54 +0200

    Update Bi_streamlit.py

--- Bi_streamlit.py
+++ Bi_streamlit.py
@@ -29,9 +29,24 @@ choice = st.sidebar.radio(
     "Select dataset",
     ('WEB','APP','TEST'))
 
+uploaded_file = st.file_uploader("Choose a file")
 
-# menu2=["A", "B"]
-# choice2=st.sidebar.selectbox("Choose dataset", menu2)
+if uploaded_file is not None:
+    # To read file as bytes:
+    bytes_data = uploaded_file.getvalue()
+    st.write(bytes_data)
+
+    # To convert to a string based IO:
+    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    st.write(stringio)
+
+    # To read file as string:
+    string_data = stringio.read()
+    st.write(string_data)
+
+    # Can be used wherever a "file-like" object is accepted:
+    dataframe = pd.read_csv(uploaded_file)
+    st.write(dataframe)
 
 
 # st. set_page_config(layout="wide")
