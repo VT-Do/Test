d8588ff389023d3edc3486eacf570fd3b6be7333
VT-Do <115161122+VT-Do@users.noreply.github.com>
2022-10-08 10:08:22 +0200

    Update Bi_streamlit.py

--- Bi_streamlit.py
+++ Bi_streamlit.py
@@ -33,8 +33,8 @@ uploaded_file = st.file_uploader("Choose a file")
 
 if uploaded_file is not None:
     # To read file as bytes:
-    bytes_data = uploaded_file.getvalue()
-    st.write(bytes_data)
+    #bytes_data = uploaded_file.getvalue()
+    #st.write(bytes_data)
 
     # Can be used wherever a "file-like" object is accepted:
     dataframe = pd.read_csv(uploaded_file)
