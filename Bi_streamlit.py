b0a945bd6da3ac025455c845c2b07f1ff9d60bea
VT-Do <115161122+VT-Do@users.noreply.github.com>
2022-10-10 16:21:31 +0200

    Update Bi_streamlit.py

--- Bi_streamlit.py
+++ Bi_streamlit.py
@@ -27,7 +27,7 @@ if uploaded_file is not None:
     # Can be used wherever a "file-like" object is accepted:
     dataframe = pd.read_csv(uploaded_file)
     
-    st.write('Hello',dataframe)
+    st.write('Uploaded data',dataframe)
 
 
 
