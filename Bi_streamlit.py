d47dcf5c234cb96d41cf9ca933cdd2931047ef59
VT-Do <115161122+VT-Do@users.noreply.github.com>
2022-10-18 12:57:17 +0200

    Update Bi_streamlit.py

--- Bi_streamlit.py
+++ Bi_streamlit.py
@@ -126,10 +126,10 @@ query_time1="SELECT A FROM `showheroes-bi.bi.Test` limit 1"
 df_time1= client.query(query_time1).to_dataframe()
 
 Time1=df_time1['A'][0]
-Time1=='AAA'
+Time1='AAA'
 st.write(Time1)
 
-if Time1='AAA':
+if Time1=='AAA':
     caching.clear_memo_cache()
 
 query_time2="SELECT Date FROM `showheroes-bi.bi.bi_appadstxt_join_sellersjson_with_count_domains` limit 1"
