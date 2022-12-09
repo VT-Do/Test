fc0e4a7f7d9bb7e87d572c94e9903a6c352c42bd
VTDo <115161122+VT-Do@users.noreply.github.com>
2022-12-09 17:00:15 +0100

    Update Test.py

--- Test.py
+++ Test.py
@@ -68,7 +68,7 @@ if st.session_state["authentication_status"]:
             query_job1 = client.query(query1)
             return client.query(query1).to_dataframe().fillna('-')
 
-        st.dataframe(load_data1('A').reset_index(drop=True),2000,2000)
+        st.dataframe(load_data1('A').reset_index(drop=True),1800,1800)
     with tab2: 
         text=[]
         if domainname !='':
@@ -93,7 +93,7 @@ if st.session_state["authentication_status"]:
             # df['Domain']=df['Domain'].str.replace('\"','')
             df['Domain'] = df['Domain'].str.lower()
 	
-            st.dataframe(df.reset_index(drop=True),2000,2000)
+            st.dataframe(df.reset_index(drop=True),1800,1800)
 	
     with tab3:
         @st.cache(max_entries=1)
@@ -102,7 +102,7 @@ if st.session_state["authentication_status"]:
             query_job2 = client.query(query2)
             return client.query(query2).to_dataframe().fillna('-')
 
-        st.dataframe(load_data2('A').reset_index(drop=True),2000,2000)
+        st.dataframe(load_data2('A').reset_index(drop=True),1800,1800)
 
 
 
